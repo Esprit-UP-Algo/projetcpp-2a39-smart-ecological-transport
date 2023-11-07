aba7356b286df93e932da97ab83b2daf3819a197
nacefmoula <132029303+nacefmoula@users.noreply.github.com>
2023-11-07 09:57:05 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
-#include <iostream>
+#include "modulevoiture.h"
+#include <QApplication>
+#include <QMessageBox>
+#include "connexion.h"
 
-using namespace std;
-
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+
+    Connection c;
+w.show();
+
+bool test=c.createconnect();
+
+if(test)
+{
+
+w.show();
+    QMessageBox::information(nullptr, QObject::tr("database is open"),
+                QObject::tr("connection successful.\n"
+                            "Click ok to start."), QMessageBox::Ok);
+
 }
+else
+    QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                QObject::tr("connection failed.\n"
+                            "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
+
+return a.exec();
+}
+
